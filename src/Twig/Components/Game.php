a10104d41e7526a446d548a622be8d9f06df90bc
sebcriado <contact.sebastiencriado@gmail.com>
2024-01-13 15:32:20 +0100

    initial commit

---
+++ src/Twig/Components/Game.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+
+#[AsLiveComponent]
+final class Game
+{
+    use DefaultActionTrait;
+
+    #[LiveProp()]
+    public array $asteroids = [];
+
+    public function __construct()
+    {
+        if(empty($this->asteroids)){
+            $this->asteroids[0] = rand(0, 9);
+        }
+    }
+
+    #[LiveAction]
+    public function addAsteroid()
+    {
+        array_unshift($this->asteroids, rand(0, 9));
+
+        if(count($this->asteroids) > 10){
+            array_pop($this->asteroids);
+        }
+    }
+
+
+}
